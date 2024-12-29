6b1de2cf6ac1fbe46f98fa1038e12db35aadb27c
Facundo <facundomoriconi19@gmail.com>
2024-12-29 12:48:16 -0300

    commit

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,19 @@
-import { CorsOptions } from 'cors'
+import { CorsOptions } from 'cors';
 
-export const corsConfig : CorsOptions = {
-    origin: function(origin, callback) {
-      const whiteList = [process.env.FRONTEND_URL] 
-      
-      if(process.argv[2] === '--api'){
-         whiteList.push(undefined)
-      }
+export const corsConfig: CorsOptions = {
+    origin: function (origin, callback) {
+        const whiteList = [process.env.FRONTEND_URL];
 
-      if (whiteList.includes(origin) ){
-          callback(null, true)
-       }else{
-         callback(new Error('No permitido por CORS'))
-       }
-   }
-}
+        // Permitir solicitudes sin origin (Postman, servicios internos) en entornos locales
+        if (process.env.NODE_ENV === 'development' || origin === undefined) {
+            return callback(null, true);
+        }
+
+        if (whiteList.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+
+   };
