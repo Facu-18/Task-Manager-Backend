808649545f307f952f2dc4994379005f83f88a07
Facundo <facundomoriconi19@gmail.com>
2024-12-29 13:06:24 -0300

    commit

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,13 +2,18 @@ import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whiteList = [process.env.FRONTEND_URL];
+        const whiteList = [
+            process.env.FRONTEND_URL, // URL de tu frontend en Netlify
+        ];
 
-        // Permitir solicitudes sin 'origin' (Postman, herramientas internas)
+        // Permitir solicitudes sin origen (por ejemplo, desde Postman)
         if (!origin || whiteList.includes(origin)) {
             return callback(null, true);
         }
 
         callback(new Error('No permitido por CORS'));
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    credentials: true, // Permitir cookies y headers con credenciales
 };
